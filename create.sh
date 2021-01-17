81bbdcdf2365ab6712975e0c8a8e1c890bd38648
Hugoo GB <hugogaben8.02@gmail.com>
2021-01-17 04:13:56 +0100

    updated create script

--- create.sh
+++ create.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-SOURCE_DIR=$HOME/projectAutoInit/
+SOURCE_DIR=$HOME/projectAutoInit
 SSH_DIR=$HOME/.ssh
 
 create() {
   cd $HOME
   source $HOME/.env
-  python ${SOURCE_DIR}create.py $1
-  cd ${PROJECTS_PATH}${1}
+  python $SOURCE_DIR/create.py $1
+  cd $PROJECTS_PATH$1
   git init
   echo "# $1" > README.md
   echo "/*" > .gitignore
@@ -16,9 +16,9 @@ create() {
   git add README.md .gitignore
 
   if [ -d $SSH_DIR ]; then
-    git remote add origin git@github.com:${USERNAME}/${1}.git
+    git remote add origin git@github.com:$USERNAME/$1.git
   else
-    git remote add origin https://github.com/${USERNAME}/${1}.git
+    git remote add origin https://github.com/$USERNAME/$1.git
   fi
 
   git commit -m "Initial commit"
